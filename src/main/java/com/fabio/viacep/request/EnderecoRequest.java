c4f38a32626f7e8a2091b106fc0be2d62506e7a6
Fabio Santos Oliveira <fabio.oliveira.prestador>
2023-07-03 16:30:41 -0300

    att

--- src/main/java/com/fabio/viacep/request/EnderecoRequest.java
+++ src/main/java/com/fabio/viacep/request/EnderecoRequest.java
@@ -6,15 +6,10 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-/*@Entity*/
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EnderecoRequest {
-
- /*   @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "idEndereco")
-    private Long id;*/
     private String cep;
 }
